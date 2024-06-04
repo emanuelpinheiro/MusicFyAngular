9ecc9bb072556ffe2435908a80d022d7e4891bcb
davisvenancio <bessaemily24@gmail.com>
2024-06-04 20:36:20 -0300

    Create cliente.service.ts

---
+++ src/app/services/cliente.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cliente } from '../models/cliente.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+  private baseUrl = 'http://localhost:8080/clientes';
+
+  constructor(private httpClient: HttpClient) {  }
+
+  findAll(): Observable<Cliente[]> {
+    return this.httpClient.get<Cliente[]>(this.baseUrl);
+  }
+
+  findById(id: string): Observable<Cliente> {
+    return this.httpClient.get<Cliente>(`${this.baseUrl}/${id}`);
+  }
+
+  insert(cliente: Cliente): Observable<Cliente> {
+    return this.httpClient.post<Cliente>(this.baseUrl, cliente);
+  }
+  
+  update(cliente: Cliente): Observable<Cliente> {
+    return this.httpClient.put<Cliente>(`${this.baseUrl}/${cliente.id}`, cliente);
+  }
+
+  delete(cliente: Cliente): Observable<any> {
+    return this.httpClient.delete<any>(`${this.baseUrl}/${cliente.id}`);
+  }
+}
