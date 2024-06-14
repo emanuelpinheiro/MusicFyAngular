ac198daf2e8d6c77f7e3359c409b4882dbd8f185
davisvenancio <venanciodavid11@gmail.com>
2024-06-13 21:46:01 -0300

    subindo branch emily

---
+++ src/app/components/login/login-admin/login-admin/login-admin.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AuthService } from '../../../../services/auth.service';
+import { NgIf } from '@angular/common';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+@Component({
+  selector: 'app-login-admin',
+  standalone: true,
+  imports: [NgIf, ReactiveFormsModule, MatFormFieldModule,
+    MatInputModule, MatButtonModule, MatCardModule, MatToolbarModule,
+    RouterModule, MatGridListModule],
+  templateUrl: './login-admin.component.html',
+  styleUrl: './login-admin.component.css'
+})
+export class LoginAdminComponent implements OnInit {
+
+  loginForm!: FormGroup;
+
+  constructor(
+     private formBuilder: FormBuilder,
+     private authService: AuthService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) { }
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(3)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const email = this.loginForm.get('email')!.value;
+      const password = this.loginForm.get('password')!.value;
+      this.authService.loginAdmin(email, password).subscribe({
+        next: (resp) => {
+          console.log("🚀 ~ LoginComponent ~ this.authService.login ~ resp:", resp)
+          // redirecionar para a página principal
+          this.router.navigateByUrl('/admin');
+        },
+        error: (err) => {
+          console.log(err);
+          this.showSnackbarTopPosition("Usuário ou senha Inválidos", 'Fechar', 2000);
+        }
+      });
+    } else {
+      this.showSnackbarTopPosition("Dados inválidos", 'Fechar', 2000);
+    }
+  }
+
+  onRegister() {
+    // criar usuário
+  }
+
+  showSnackbarTopPosition(content: any, action: any, duration: any) {
+    this.snackBar.open(content, action, {
+      duration: 2000,
+      verticalPosition: "top", // Allowed values are  'top' | 'bottom'
+      horizontalPosition: "center" // Allowed values are 'start' | 'center' | 'end' | 'left' | 'right'
+    });
+  }
+
+  cadastrar() {
+    this.router.navigateByUrl('/cadastro');
+  }
+
+}
